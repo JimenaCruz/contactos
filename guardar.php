55fcf9ce589bfa2bc02275a38790d4b841d4f9bb
JimenaCruz <62568674+JimenaCruz@users.noreply.github.com>
2020-05-27 13:45:38 -0600

    si

--- guardar.php
+++ guardar.php
@@ -1,33 +1,35 @@
->?php
-$conectar=@mysql_connect('localhost','root','');
-if(!$conectar)
-{
-	echo"No se pudo conectar con el Servidor"; 
-}
-else{
-	$base=mysql_select_db('prueba'); 
-	if (!$base)
-	{
-		echo"No se encontró la base de datos";
-	}
-}
-$nombre=$_POST['nombre'];
-$correo=$_POST['correo'];
-$mensaje=$_POST['mensaje'];
+ <?php
+ //conectamos con el servidor
+		$conectar=@mysql_connect('localhost','root','')
+		if(!$conectar)
+		{
+			echo"No se pudo conectar con el Servidor"; 
+		}
+		else{
+			$base=mysql_select_db('prueba'); 
+			if (!$base)
+			{
+				echo"No se encontró la base de datos";
+			}
+		}
+		//recuperamos variables
+		$nombre=$_POST['nombre'];
+		$correo=$_POST['correo'];
+		$mensaje=$_POST['mensaje'];
+		//sentencia de sql
+		$sql="INSERT INTO datos VALUES ('$nombre',
+										'$correo',
+										'$mensaje')";
+			//ejecutamos la sentencia de sql
+		$ejecutar=mysql_query($sql);
+		//verificamos la sentencia de sql
+		if(!$ejecutar)
+		{
+			echo "Hubo Algun Error";
 
-$sql="INSERT INTO datos VALUES ('$nombre',
-								'$correo',
-								'$mensaje')";
+		}
+		else {
+			echo"Datos Guardados Correctamente <br><a href='index.html'>Volver</a>";
 
-$ejecutar=mysql_query($sql);
-
-if(!$ejecutar)
-{
-	echo"Hubo Algun Error";
-
-}
-else {
-	echo"Datos Guardados Correctamente <br><a href='index.html'>Volver</a>";
-
-}
-?>
+		}
+		?>
