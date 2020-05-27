d0b3de99cad64ebc1b4e3ffbcba721243ec63cd7
JimenaCruz <62568674+JimenaCruz@users.noreply.github.com>
2020-05-27 14:28:41 -0600

    si

--- guardar.php
+++ guardar.php
@@ -1,35 +1,25 @@
  <?php
- //conectamos con el  servidor
-		$conectar=@mysql_connect('localhost','root','')
-		if(!$conectar)
-		{
-			echo"No se pudo conectar con el Servidor"; 
-		}
-		else{
-			$base=mysql_select_db('prueba'); 
-			if (!$base)
-			{
-				echo"No se encontró la base de datos";
-			}
-		}
-		//recuperamos variables
-		$nombre=$_POST['nombre'];
-		$correo=$_POST['correo'];
-		$mensaje=$_POST['mensaje'];
-		//sentencia de sql
-		$sql="INSERT INTO datos VALUES ('$nombre',
-										'$correo',
-										'$mensaje')";
-			//ejecutamos la sentencia de sql
-		$ejecutar=mysql_query($sql);
-		//verificamos la sentencia de sql
-		if(!$ejecutar)
-		{
-			echo "Hubo Algun Error";
+ class Conexion extends mysqli// Conexion:mysqli 
+        {
+            
 
-		}
-		else {
-			echo"Datos Guardados Correctamente <br><a href='index.html'>Volver</a>";
+            public function __construct()
+            {
+            $servidor= "localhost";
+            $database= "prueba"; 
+            $usuario="root";
+            $password="";
+            parent::__construct($servidor, $usuario, $password, $basadatos);            
+             $this->connect_errno ? die('Error en la conexion')  : $error-"Conectado a ".$basadatos;
+             echo $error;
+             unset($error);
 
-		}
+
+             }
+        }
+
+
+
+
+
 		?>
